49913c4dc9175f5deefa78f9802266232100b459
Roman Kislitsin <r.kislitsyn@trucker.group>
2025-05-22 18:10:28 +0400

    add recursive transform logic

--- lib/hash_transformer.rb
+++ lib/hash_transformer.rb
@@ -1,9 +1,30 @@
 class HashTransformer
-  def initialize(input_hash)
-    @input_hash = input_hash
+  def initialize(input)
+    raise ArgumentError, "Input must be a Hash, got #{input.class}" unless input.is_a?(Hash)
+    @input = input
   end
 
   def call
-    {} # Стаб
+    transform(@input)
+  end
+
+  private
+
+  def transform(value)
+    case value
+    when Hash
+      value.each_with_object({}) do |(k, v), result|
+        result[k] = transform(v)
+      end
+    when Array
+      value.each_with_object({}) do |v, result|
+        transformed = transform(v)
+        transformed.each { |k, v2| result[k] = v2 }
+      end
+    when Symbol, String, Numeric
+      { value => {} }
+    else
+      raise ArgumentError, "Unsupported type #{value.class} for value: #{value.inspect}"
+    end
   end
 end
